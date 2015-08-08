fd9c44b43845a5df8466eaed03cf65b354aedd4d
Kim <kimhayden@Kims-MacBook-Air.local>
2015-08-08 13:26:05 -0500

    week 6 resume

---
+++ resume.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$(".slidingDiv").hide();
+$(".show_hide").show();
+
+$('.show_hide').click(function(){
+$(".slidingDiv").slideToggle();
+});
+
+});
+
+//modified from http://papermashup.com/demos/jquery-sliding-div/
